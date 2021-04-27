59a9b25607a6403d3b5e11062c2ee9daf26a9f46
Ben Smith <p52smith@outlook.com>
2021-04-27 20:02:21 +0100

    Move rarity enumeration definition to own file
    This allows reuse amongst multiple items types and simplifies naming conventions

---
+++ Warframe Gear Tracker/WarframeRarity.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warframe_Gear_Tracker
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum WarframeRarity
+    {
+        Unknown,
+        Common,
+        Uncommon,
+        Rare,
+        Legendary
+    }
+}
--- Warframe Gear Tracker/WarframeRelicReward.cs
+++ Warframe Gear Tracker/WarframeRelicReward.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +8,7 @@ namespace Warframe_Gear_Tracker
 {
     public class WarframeRelicReward
     {
-        [JsonConverter(typeof(StringEnumConverter))]
-        public enum Rarity
-        {
-            Common,
-            Uncommon,
-            Rare
-        }
-
-        public Rarity rarity { get; set; }
+        public WarframeRarity Rarity { get; set; }
         public string RewardName { get; set; }
     }
 }
